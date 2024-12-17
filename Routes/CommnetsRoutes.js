73e506eae283e0c4f8ecf34541ce0d0fe470dde0
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-12-17 12:23:36 +0300

    updated on NackEnd Basma

---
+++ Routes/CommnetsRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const commentController = require('../Controllers/CommentsController');
+const rateLimiter = require('../MiddleWares/rateLimiter');
+const authMiddleware = require('../MiddleWares/authMiddleware');  
+
+
+router.post('/addComment', rateLimiter, commentController.addComment);
+
+
+router.get('/getComments', rateLimiter, commentController.getComments);
+
+
+router.get('/getComment/:id', rateLimiter, commentController.getCommentById);
+
+
+router.put('/updateComment/:id', rateLimiter, commentController.updateComment);
+
+
+router.delete('/deleteComment/:id', rateLimiter, commentController.deleteComment);
+
+module.exports = router;
